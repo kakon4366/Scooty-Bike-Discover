1135c34bd1f33d05fa8faaeef9ae69d5430358c0
kakon4366 <kakonbarman4366@gmail.com>
2022-04-03 23:33:45 +0600

    Routes set and Not Found Page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ContactUs from "./Components/ContactUs/ContactUs";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import NotFound404 from "./Components/NotFound404/NotFound404";
+import Review from "./Components/Review/Review";
 
 function App() {
 	return (
 		<div>
 			<Header></Header>
-
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/home" element={<Home />}></Route>
+				<Route path="/review" element={<Review />}></Route>
+				<Route path="/dashboard" element={<Dashboard />}></Route>
+				<Route path="/contact-us" element={<ContactUs />}></Route>
+				<Route path="*" element={<NotFound404 />}></Route>
+			</Routes>
 			<Footer></Footer>
 		</div>
 	);
---
+++ src/Components/NotFound404/NotFound404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound404 = () => {
+	return (
+		<div className="my-12 container mx-auto text-center">
+			<h2 className="text-4xl">This page is not available!!!</h2>
+			<h1 className="text-8xl">404</h1>
+		</div>
+	);
+};
+
+export default NotFound404;
